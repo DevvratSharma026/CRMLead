21438ffc0f60071bfa24bdfe2a67c4104ad04cce
Devvrat Sharma <devsharma.pcm.2003@gmail.com>
2026-01-15 18:44:59 +0530

    made server.js and db.js for connection

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+connectDB();
+
+app.get('/api/health', (req, res) => {
+    res.status(200).json({
+        success: true,
+        status: "ok"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
