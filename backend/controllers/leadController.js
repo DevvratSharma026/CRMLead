b247653f7d30ca47119fc4c7280f17eaec6e0e55
Devvrat Sharma <devsharma.pcm.2003@gmail.com>
2026-01-15 21:27:09 +0530

    made the getleads controllers

---
+++ backend/controllers/leadController.js
@@ -0,0 +1,47 @@
+import Lead from "../models/Lead.js";
+
+export const getLeads = async (req, res) => {
+    try {
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const search = req.query.search || "";
+        const status = req.query.status;
+        const stage = req.query.stage;
+        const sort = req.query.sort || "createdAt";
+        const order = req.query.order === "asc" ? 1 : -1;
+        const query = {};
+
+        if (search) {
+            query.$or = [
+                { name: { $regex: search, $options: "i" } },
+                { email: { $regex: search, $options: "i" } },
+            ];
+        }
+
+        if (status) {
+            query.status = status;
+        }
+        if (stage) {
+            query.stage = stage;
+        }
+
+        const skip = (page - 1) * limit;
+
+        const leads = await Lead.find(query)
+            .sort({ [sort]: order })
+            .skip(skip)
+            .limit(limit);
+        const total = await Lead.countDocuments(query);
+
+        res.json({
+            total,
+            page, 
+            limit,
+            leads,
+        });
+    } catch (err) {
+        res.status(500).json({
+            message: err.message
+        })
+    }
+}
--- backend/routes/leads.routes.js
+++ backend/routes/leads.routes.js
@@ -1,12 +1,9 @@
 import express from 'express'
+import { getLeads } from '../controllers/leadController.js';
 const router = express.Router();
 
 //GET /api/leads
-router.get('/', (req, res) => {
-    res.json({
-        message: 'Get all leads'
-    });
-});
+router.get('/', getLeads);
 
 //GET /api/leads/:id
 router.get('/:id', (req, res) => {
