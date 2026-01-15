95e20367ceae61c7335252a51dd31f9396df874f
Devvrat Sharma <devsharma.pcm.2003@gmail.com>
2026-01-15 21:32:00 +0530

    fetch by id is done

--- backend/controllers/leadController.js
+++ backend/controllers/leadController.js
@@ -45,3 +45,24 @@ export const getLeads = async (req, res) => {
         })
     }
 }
+
+export const getLeadById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const lead = await Lead.findById(id);
+
+        if(!lead) {
+            return res.status(404).json({
+                message: "No lead found with this Id"
+            });
+        }
+
+        res.json({
+            lead
+        })
+    } catch(err) {
+        res.status(500).json({
+            message: "Invalid Lead Id"
+        })
+    }
+}
--- backend/routes/leads.routes.js
+++ backend/routes/leads.routes.js
@@ -1,15 +1,11 @@
 import express from 'express'
-import { getLeads } from '../controllers/leadController.js';
+import { getLeadById, getLeads } from '../controllers/leadController.js';
 const router = express.Router();
 
 //GET /api/leads
 router.get('/', getLeads);
 
 //GET /api/leads/:id
-router.get('/:id', (req, res) => {
-    res.json({
-        message: `Get lead with id ${req.params.id}`
-    });
-});
+router.get('/:id', getLeadById);
 
 export default router;
